1125972bb8a392b24fe532ec87b967e3f3496409
Hang T <tp.hang92@gmail.com>
2017-10-13 22:34:22 -0400

    Update 2.5.py

--- 2.5.py
+++ 2.5.py
@@ -1,4 +1,7 @@
-#2.5
+"""
+Function casual_number(s) should return an integer representing a number in s. If s does not look like a number the function should 
+return None. Take out the commas that separate the thousands.
+"""
 def casual_number(s):
     if any(c.isalpha() for c in s): #if any character in the string is a letter, print "None"
         print ("None")
