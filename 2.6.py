ef53da7c8fb1ebb92332c65847b2f654e29d5636
Hang T <tp.hang92@gmail.com>
2017-10-13 22:30:16 -0400

    Add files via upload

---
+++ 2.6.py
@@ -0,0 +1,13 @@
+#2.6
+def alienNumbers(s):
+    alienNumbers = str(s) #s must be a string
+    return 1024*alienNumbers.count("T") + 598*alienNumbers.count("y") + 121*alienNumbers.count("!") + 42*alienNumbers.count("a") + 6*alienNumbers.count("N") + 1*alienNumbers.count("U")
+
+"""
+Return 1024 for each T
+Return 598 for each y
+Return 121 for each !
+Return 42 for each a
+Return 6 for each N
+Return 1 for each U
+"""
